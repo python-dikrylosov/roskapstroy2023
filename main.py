6abd6dc8198cd7836a9e8474016ceb3844cb6845
python_dikrylosov <48491372+python-dikrylosov@users.noreply.github.com>
2023-06-25 09:00:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,58 @@ import re
 from nltk.corpus import stopwords
 filename = "Датасет.xlsx"
 data = pd.read_excel(filename)
-print(data)
-print(data.shape)
+#print(data)
+#print(data.shape)
 sdfgdfg = ["Арматурщик","Инженер","Слесарь-Ремонтник"]
 for i in range(data.shape[0]):
+
+        import requests
+        import bs4
+
+        cookies = {
+                '_ym_uid': '1616661207763271544',
+                '_ym_d': '1663424427',
+                '__ddg1_': '83Ffnluv63rwe2MHhPNO',
+                'tmr_lvid': 'ec909d6bd0626bcb7353ff87094f443c',
+                'tmr_lvidTS': '1616661206317',
+                '_ga': 'GA1.2.1283363942.1666370669',
+                'tmr_detect': '1%7C1667230005197',
+                '_gid': 'GA1.2.1931147048.1667230005',
+                '_ym_isad': '1',
+                'tmr_reqNum': '337',
+        }
+
+        headers = {
+                'authority': 'pln-pskov.ru',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                # Requests sorts cookies= alphabetically
+                # 'cookie': '_ym_uid=1616661207763271544; _ym_d=1663424427; __ddg1_=83Ffnluv63rwe2MHhPNO; tmr_lvid=ec909d6bd0626bcb7353ff87094f443c; tmr_lvidTS=1616661206317; _ga=GA1.2.1283363942.1666370669; tmr_detect=1%7C1667230005197; _gid=GA1.2.1931147048.1667230005; _ym_isad=1; tmr_reqNum=337',
+                'referer': 'https://pln-pskov.ru/',
+                'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        }
+
+        data_link_resource = data["link_resource"][i]
+        print(data_link_resource)
+        response = requests.get(data_link_resource,cookies=cookies, headers=headers)
+        print(response)
+        news = bs4.BeautifulSoup(response.text)
+        print(news)
+        links = news.find_all({'class': "g-user-content"},{'data-qa':'vacancy-description'})
+        print(links)
+        for i in range(4):
+                time.sleep(1)
+                print(i)
+
+
         print("\n\n",i,data["id"][i],"\n",          data["name(название)"][i],"\n",data["responsibilities(Должностные обязанности)"][i],"\n",)
 
         ru_text = str(data["responsibilities(Должностные обязанности)"][i])
@@ -96,7 +144,7 @@ for i in range(data.shape[0]):
         cos = textdistance.cosine(T1, T2)
         print(f'Тексты схожи на {100 * cos}%.')
         if (100 * cos) > 0:
-                print("Условия есть")
+                print("Условия есть" )
                 time.sleep(10)
         elif (100 * cos) == 0:
                 print("Условий не обнаружено")
